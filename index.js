e23bd2d30836ec1bfc3d14ad2bafcd32ed1b9c21
Jakob Löhnertz <mail@jakob.codes>
2019-04-11 23:40:20 +0200

    Polish the whole application

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const Counter = require('prom-client').Counter;
 
 
 // Constants
-const Port = 3015;
+const PortKey = 'port';
 const EpisodeHostKey = 'episodeHost';
 const EpisodeBasePathKey = 'episodeBasePath';
 const EpisodeBaseNameKey = 'episodeBaseName';
 const EpisodeFileExtensionKey = 'episodeFileExtension';
 
 // Initializations
+const config = JSON.parse(fs.readFileSync('config.json'));
+
 const server = express();
 
 const counterDownloads = new Counter({
@@ -26,12 +28,8 @@ const counterDownloads = new Counter({
 require('prom-client').collectDefaultMetrics();
 
 
-// Helpers
-const config = JSON.parse(fs.readFileSync('config.json'));
-
-
 // Endpoints
-server.use('/episodes/:number', proxy(config[EpisodeHostKey], {
+server.use('/proxy/episodes/:number', proxy(config[EpisodeHostKey], {
     proxyReqPathResolver: (req) => {
         const episodeBasePath = config[EpisodeBasePathKey];
         const episodeBaseName = config[EpisodeBaseNameKey];
@@ -49,13 +47,8 @@ server.get('/metrics', (req, res) => {
     res.end(register.metrics());
 });
 
-server.get('/metrics/counter', (req, res) => {
-    res.set('Content-Type', register.contentType);
-    res.end(register.getSingleMetricAsString('test_counter'));
-});
-
 
 // Startup
-console.log('Server listening on port: ' + Port);
+console.log('Server listening on port: ' + config[PortKey]);
 
-server.listen(Port);
+server.listen(config[PortKey]);
